d33e86b844521bf430b6f3dbe75d088ec6d3d797
omarelbeshbichi <94106234+omarelbeshbichi@users.noreply.github.com>
2026-01-12 17:28:58 +0100

    feat: add initial frontend container and FastAPI server
    - Introduce frontend container for Kepler-based visualization
    - Add simple FastAPI + Uvicorn server to serve map
    - Add missing dependencies

---
+++ frontend/kepler/server.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from .map import create_map
+from cmm.utils.misc import get_project_root
+import tempfile
+
+app = FastAPI()
+
+@app.get("/", response_class = HTMLResponse)
+def serve_map():
+
+    root_path = get_project_root()
+    kepler_config_path = root_path  / "frontend" / "kepler" / "kepler_config.json"
+
+    m = create_map(kepler_config_path)
+
+    # Obtain HTML representation of kepler map (save to temp -> load from temp)
+    with tempfile.NamedTemporaryFile(suffix=".html", delete = False) as f:        
+        m.save_to_html(file_name = f.name, read_only = True)
+        html_content = f.read().decode("utf-8")
+
+    # Enforce full screen size
+    style_fix = """
+    <style>
+        #kepler-gl__map {
+            width: 100vw !important;
+            height: 100vh !important;
+        }
+        body { margin: 0; overflow: hidden; }
+    </style>
+    """
+
+    html_content = style_fix + html_content
+
+    return HTMLResponse(content = html_content)
